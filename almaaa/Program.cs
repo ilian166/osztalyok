77cd3cc255bdda8575930197199345df8f6ef745
Ilián Molnár <molnar.ilian@petrik.hu>
2025-02-07 11:10:25 +0100

    konyv

--- almaaa/Program.cs
+++ almaaa/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-           
+            Book b1 = new Book("alma", "asd", 1800, 425, 5000);
+            Book b2 = new Book("alma2", "asd2", 1920, 680, 8000);
+            Book b3 = new Book("alma3", "asd3");
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+
+            Console.WriteLine(b1.PriceInc(500));
+
+            Console.WriteLine(b2.Discount(10));
+          
+            Console.WriteLine(b3.RemainingPages(148));
         }
     }
 }
--- almaaa/book.cs
+++ almaaa/book.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace almaaa
 {
-    internal class book
+    internal class Book
     {
+        private string title;
+        private string author;
+        private int releaseDate;
+        private int pageNumber;
+        private int price;
+
+        
+        public Book(string title, string auther, int releaseDate, int pageNumber, int price)
+        {
+            this.title = title;
+            this.author = auther;
+            this.releaseDate = releaseDate;
+            this.pageNumber = pageNumber;
+            this.price = price;
+        }
+
+        public Book(string title, string auther)
+        {
+            this.title = title;
+            this.author = auther;
+            this.releaseDate = DateTime.Now.Year;
+            this.pageNumber = 200;
+            this.price = 3000;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public int ReleaseDate { get => releaseDate; set => releaseDate = value; }
+        public int PageNumber { get => pageNumber; set => pageNumber = value; }
+        public int Price { get => price; set => price = value; }
+
+
+        public int PriceInc(int increase)
+        {
+            return this.price + increase;
+        }
+
+        public double Discount(double percent)
+        {
+            return this.price - (this.price * (percent / 100));
+        }
+
+        public int RemainingPages(int read)
+        {
+            return this.pageNumber - read;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{title} - {author} / {pageNumber} / {releaseDate} - {price}";
+        }
+
     }
 }
