1af553e99d36081858f050a627ef5f7fb10b069b
Ilián Molnár <molnar.ilian@petrik.hu>
2025-02-07 12:22:37 +0100

    film

---
+++ almaaa/Movie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace almaaa
+{
+    internal class Movie
+    {
+        private string title;
+        private string diractor;
+        private int lengthMin;
+        private string type;
+        private bool release;
+
+
+        public Movie(string title, string diractor, int lengthMin, string type, bool release)
+        {
+            this.title = title;
+            this.diractor = diractor;
+            this.lengthMin = lengthMin;
+            this.type = type;
+            this.release = release;
+        }
+
+        public Movie(string title, string diractor)
+        {
+            this.title = title;
+            this.diractor = diractor;
+            this.lengthMin = 120;
+            this.type = "ismeretlen";
+            this.release = false;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Diractor { get => diractor; set => diractor = value; }
+        public int LengthMin { get => lengthMin; set => lengthMin = value; }
+        public string Type { get => type; set => type = value; }
+        public bool Release { get => release; set => release = value; }
+
+
+
+        public string Play()
+        {
+            if (release == true)
+            {
+                return "a film elkezdődött";
+            }
+            else
+            {
+                return "a film nem kezdődött el";
+            }
+        }
+
+        public string TypeChange(string newtype)
+        {
+           return this.type = newtype;
+        }
+
+        public int LengthInc(int increase)
+        {
+            return this.lengthMin + increase;
+        }
+
+        public override string ToString()
+        {
+            return $"{title} - {diractor} / {lengthMin} / {type} - {release}";
+        }
+
+    }
+}
--- almaaa/Program.cs
+++ almaaa/Program.cs
@@ -17,6 +17,23 @@
             Console.WriteLine(b2.Discount(10));
           
             Console.WriteLine(b3.RemainingPages(148));
+
+
+            Movie f1 = new Movie("alma", "asd", 1800, "asdasd", true);
+            Movie f2 = new Movie("alma2", "asd2", 1920, "asdasd2", false);
+            Movie f3 = new Movie("alma3", "asd3");
+
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            Console.WriteLine(f3);
+
+            Console.WriteLine(f1.Play());
+
+            Console.WriteLine(f2.TypeChange("asdf"));
+
+            Console.WriteLine(f3.LengthInc(24));
         }
+
+
     }
 }
